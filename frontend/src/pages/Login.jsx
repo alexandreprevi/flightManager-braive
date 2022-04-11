f7e66f82fc307ca320d1d66607b8e17d474e5234
Alexandre Previ <alexandre.previ@gmail.com>
2022-04-11 16:09:20 +0200

    implemented login to Login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,18 @@
+import { useSelector, useDispatch } from "react-redux"
+import { useNavigate } from "react-router-dom"
+
 import Form from "../components/Form"
 import Input from "../components/Form/Input"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { FlexContainer } from "../components/UI/Flex.styled"
 import Button from "../components/UI/Button.styled"
-import { AiOutlineLock, AiOutlineMail } from "react-icons/ai"
+import { AiOutlineLock, AiOutlineUser } from "react-icons/ai"
+import { login, reset } from "../features/auth/authSlice"
+import { toast } from "react-toastify"
 
 const Login = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
   const [formData, setFormData] = useState({
     username: "",
     password: "",
@@ -13,13 +20,26 @@ const Login = () => {
 
   const { username, password } = formData
 
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }))
   }
-  const onSubmit = (e) => {}
+  const onSubmit = () => {
+    dispatch(login({ username, password }))
+  }
+
+  useEffect(() => {
+    if (isError) toast.error(message)
+    if (isSuccess || user) navigate("/")
+
+    dispatch(reset())
+  }, [user, isSuccess, isError, message, navigate, dispatch])
 
   return (
     <FlexContainer flexDirection="column" flexGrow={1}>
@@ -31,7 +51,7 @@ const Login = () => {
               name="username"
               value={username}
               onChange={onChange}
-              icon={<AiOutlineMail />}
+              icon={<AiOutlineUser />}
             />
           </Form.Control>
           <Form.Control label="password">
@@ -44,7 +64,7 @@ const Login = () => {
             />
           </Form.Control>
         </Form.Section>
-        <Button.Secondary size="small" type="submit">
+        <Button.Secondary size="small" type="submit" disabled={isLoading}>
           Login
         </Button.Secondary>
       </Form.Wrapper>
