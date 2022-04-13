57460add7db6ffb212d0a2fb1e58c236697caa4d
Alexandre Previ <alexandre.previ@gmail.com>
2022-04-13 10:16:28 +0200

    updated Home with search input and filters

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,10 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
+import FlightItem from "../components/FlightItem"
+import Input from "../components/Form/Input"
+import { FlexContainer } from "../components/UI/Flex.styled"
+import Text from "../components/UI/Text.styled"
 import { reset as resetAuth } from "../features/auth/authSlice"
 import {
   getAllFlights,
@@ -14,6 +18,8 @@ const Home = () => {
   const { user } = useSelector((state) => state.auth)
   const { flights, isError, isLoading } = useSelector((state) => state.flight)
 
+  const [searchValue, setSearchValue] = useState("")
+
   useEffect(() => {
     if (!user) navigate("/login")
 
@@ -25,18 +31,46 @@ const Home = () => {
     }
   }, [user, navigate, dispatch])
 
+  useEffect(() => {
+    if (searchValue) console.log(searchValue)
+  }, [searchValue])
+
   if (isError) return <>Could not load flights. Please try again</>
   if (isLoading) return <>Loading Flights...</>
 
   return (
-    <>
-      <div>Hi {user.username}</div>
+    <FlexContainer
+      flexDirection="column"
+      wrap="wrap"
+      padding="medium"
+      spacing="large"
+      flexBasis="large"
+      flexGrow={1}
+    >
+      <Text.Title>Hi {user.username}</Text.Title>
+      <Input
+        placeholder="Search..."
+        onChange={(e) => setSearchValue(e.target.value)}
+      ></Input>
       {flights && flights.length > 0 ? (
-        flights.map((flight, index) => <p key={index}>{flight.name}</p>)
+        flights
+          .filter((flight) => {
+            if (
+              flight.flightName.toLowerCase().includes(searchValue) ||
+              flight.scheduledDateAndTime.toLowerCase().includes(searchValue) ||
+              flight.departureFrom.toLowerCase().includes(searchValue) ||
+              flight.destination.toLowerCase().includes(searchValue)
+            ) {
+              return true
+            } else {
+              return false
+            }
+          })
+          .map((flight) => <FlightItem key={flight._id} flight={flight} />)
       ) : (
         <>No flights to display</>
       )}
-    </>
+    </FlexContainer>
   )
 }
 
