f5d55bf64ee30fcbe05f71016067b10446d3823a
Alexandre Previ <alexandre.previ@gmail.com>
2022-04-13 17:41:59 +0200

    created NotFound page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,27 @@
 import React from "react"
+import Button from "../components/UI/Button.styled"
+import { FlexContainer } from "../components/UI/Flex.styled"
+import { useNavigate } from "react-router-dom"
+import Text from "../components/UI/Text.styled"
 
 const NotFound = () => {
-  return <div>NotFound</div>
+  const navigate = useNavigate()
+  return (
+    <FlexContainer
+      flexDirection="column"
+      spacing="medium"
+      alignItems="center"
+      padding={"extraLarge"}
+    >
+      <Text.Title align="center" color="mediumFill">
+        Page Not Found
+      </Text.Title>
+
+      <Button.Primary size="large" onClick={() => navigate("/")}>
+        <Text.Body>Go Home</Text.Body>
+      </Button.Primary>
+    </FlexContainer>
+  )
 }
 
 export default NotFound
